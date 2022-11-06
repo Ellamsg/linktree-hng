cca2ca3d4a20263650aaa949bb83e5fce9a3bf31
ellamsg <ellamsgeorge7@gmail.com>
2022-11-06 01:08:16 -0700

    route

--- src/components/Main.js
+++ src/components/Main.js
@@ -74,7 +74,7 @@ export default function Main() {
           id="Contact"
           to="/Contact"
         >
-         Contact
+         Contact 
         </Link>
       </section>
       {/*section logo*/}
